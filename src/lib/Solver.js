caaee76f2c8134751a8c72613f8e7c39d2c973ef
Mike McBride <mmcbride1007@icloud.com>
2020-01-12 01:56:00 -0600

    adds detours and flattens grid for pass

--- src/lib/Solver.js
+++ src/lib/Solver.js
@@ -13,6 +13,7 @@ export default class Solver {
     this.snapshots = []
     this.startTime = 0
     this.finishTime = 0
+    this.detours = []
   }
 
   start() {
@@ -120,52 +121,134 @@ export default class Solver {
     this.calculations += 1
   }
 
+  setDetoursForSolvedCell({ row, column, value }) {
+    const unsolved = [
+      ...this.getCellsInColumn(column),
+      ...this.getCellsInRow(row),
+      ...this.getCellsInSquare(row, column)
+    ].filter(it => !it.solved)
+    
+    unsolved.forEach(it => {
+      const id = `${it.row}.${it.column}`
+      const detour = {
+        row: it.row,
+        column: it.column,
+        id: id
+      }
+
+      const isInDetours = this.detours.some(it => it.id === id)
+
+      // we know the value of the solved cell, so update this cell's possible values to remove it.
+      const remoteCell = this.grid[it.row][it.column]
+      remoteCell.possibleValues = remoteCell.possibleValues.filter(val => val !== value)
+
+      if (remoteCell.possibleValues.length === 1) {
+        // we just solved another cell. set it to solved and don't add to our detours array.
+        remoteCell.value = remoteCell.possibleValues[0]
+        remoteCell.solved = true
+
+        if (isInDetours) {
+          // this cell was already in detours, but we just solved it. remove it.
+          this.detours = this.detours.filter(d => d.id !== id)
+        }
+        this.setDetoursForSolvedCell(remoteCell)
+      } else if (!isInDetours) {
+        // this item isn't in our array yet. add it
+        this.detours.push(detour)
+      }
+    })
+  }
+
+  getFlattenedGrid() {
+    // returns a flattened array for enabling single-pass loop through all grid cells
+    return this.grid.reduce((acc, row) => {
+      return [...acc, ...row]
+    }, [])
+  }
+
   makePass() {
     let hasUpdatedValues = false
     let hasReverted = false
+    const cells = this.getFlattenedGrid()
 
     // pass through each cell and check for possible values
     // if it only has one possible, set it and mark it solved
     // otherwise, just update possible values and move on
-    outer_loop: for (let row in this.grid) {
-      let gridRow = this.grid[row]
-      for (let col in gridRow) {
-        let cell = this.grid[row][col]
+    for (let cell of cells) {
+      const row = cell.row
+      const col = cell.column
 
-        if (cell.solved === false) {
-          if (cell.possibleValues.length === 0) {
-            // we ran out of options here. We need to revert one more snapshot back before proceeding.
-            this.revertSnapshot()
-            this.incrementCalculations()
-            hasReverted = true
-            break outer_loop
-          }
-
-          let possibleValues = this.getPossibleCellValues(row, col)
+      if (cell.solved === false) {
+        if (cell.possibleValues.length === 0) {
+          // we ran out of options here. We need to revert one more snapshot back before proceeding.
+          this.revertSnapshot()
           this.incrementCalculations()
+          hasReverted = true
+          break
+        }
 
-          if (possibleValues.length === 0) {
-            // no possible values, this attempt is not correct. bail.
-            this.revertSnapshot()
-            this.incrementCalculations()
-            hasReverted = true
-            break outer_loop
-          }
+        let possibleValues = this.getPossibleCellValues(row, col)
+        this.incrementCalculations()
 
-          if (possibleValues.length < cell.possibleValues.length) {
-            // this is a situation we can get ourselves in when reverting a snapshot.
-            // if we have eliminated some possible values by trying them, we shouldn't
-            // revert that knowledge. Only update cell.possibleValues if it benefits us.
-            hasUpdatedValues = true
-            cell.possibleValues = possibleValues
-            this.incrementCalculations()
-          }
+        if (possibleValues.length === 0) {
+          // no possible values, this attempt is not correct. bail.
+          this.revertSnapshot()
+          this.incrementCalculations()
+          hasReverted = true
+          break
+        }
 
-          if (cell.possibleValues.length === 1) {
-            cell.solved = true
-            cell.value = cell.possibleValues[0]
-            this.incrementCalculations()
-          }
+        if (possibleValues.length < cell.possibleValues.length) {
+          // this is a situation we can get ourselves in when reverting a snapshot.
+          // if we have eliminated some possible values by trying them, we shouldn't
+          // revert that knowledge. Only update cell.possibleValues if it benefits us.
+          hasUpdatedValues = true
+          cell.possibleValues = possibleValues
+          this.incrementCalculations()
+        }
+
+        if (cell.possibleValues.length === 1) {
+          cell.solved = true
+          cell.value = cell.possibleValues[0]
+          this.incrementCalculations()
+          this.setDetoursForSolvedCell(cell)
+        }
+      }
+    }
+
+    // I tried running this immediately after solving a cell and also running it after a full pass through the board.
+    // There were trade-offs both ways, but the time to solve didn't vary much, and this approach actually resulted
+    // in fewer required calculations to solve the simpler puzzles than running immediately after solving a cell.
+    while (this.detours.length > 0) {
+      const { row, column } = this.detours.shift()
+      const cell = this.grid[row][column]
+
+      if (cell.solved === false) {
+        let possibleValues = this.getPossibleCellValues(row, column)
+        this.incrementCalculations()
+
+        if (possibleValues.length === 0) {
+          // no possible values, this attempt is not correct. bail.
+          this.revertSnapshot()
+          this.incrementCalculations()
+          hasReverted = true
+          this.detours = []
+        }
+
+        if (possibleValues.length < cell.possibleValues.length) {
+          // this is a situation we can get ourselves in when reverting a snapshot.
+          // if we have eliminated some possible values by trying them, we shouldn't
+          // revert that knowledge. Only update cell.possibleValues if it benefits us.
+          hasUpdatedValues = true
+          cell.possibleValues = possibleValues
+          this.incrementCalculations()
+        }
+
+        if (cell.possibleValues.length === 1) {
+          cell.solved = true
+          cell.value = cell.possibleValues[0]
+          this.incrementCalculations()
+          this.setDetoursForSolvedCell(cell)
         }
       }
     }
@@ -173,30 +256,28 @@ export default class Solver {
     if (hasUpdatedValues === false && hasReverted === false) {
       // we made it through without updating anything, start making assumptions.
       // find the first unsolved cell, try the first possible value, and take a snapshot.
-      outer_loop: for (let row in this.grid) {
-        for (let col in this.grid[row]) {
-          let cell = this.grid[row][col]
+      for (let cell of cells) {
+        this.incrementCalculations()
 
+        if (cell.solved === false) {
+          const row = cell.row
+          const col = cell.column
+          // grab first item in possible values, and remove it from the array
+          let testVal = cell.possibleValues.shift()
+
+          // take a snapshot NOW, so before we update the cell but after we've removed one possible value.
+          // this will ensure we don't ever repeat checks but still test all cells properly.
+          // also make sure we do a deep copy of the grid, so we don't retain object references
+          const snapshot = klona(this.grid)
+          this.addSnapshot(snapshot)
           this.incrementCalculations()
 
-          if (cell.solved === false) {
-            // grab first item in possible values, and remove it from the array
-            let testVal = cell.possibleValues.shift()
+          // now update our value and solved
+          this.setCellValue({ row, col, val: testVal })
+          this.setCellSolved({ row, col, val: true })
+          this.incrementCalculations()
 
-            // take a snapshot NOW, so before we update the cell but after we've removed one possible value.
-            // this will ensure we don't ever repeat checks but still test all cells properly.
-            // also make sure we do a deep copy of the grid, so we don't retain object references
-            const snapshot = klona(this.grid)
-            this.addSnapshot(snapshot)
-            this.incrementCalculations()
-
-            // now update our value and solved
-            this.setCellValue({ row, col, val: testVal })
-            this.setCellSolved({ row, col, val: true })
-            this.incrementCalculations()
-
-            break outer_loop
-          }
+          break
         }
       }
     }
